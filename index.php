627351dcd33b665e0d6d107f9ba24c46ddbeb72f
cherki-hamza <cherki0hamza@gmail.com>
2021-06-28 16:33:12 +0100

    first commit of wordpress theme developments

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>cherki wp dev</title>
+</head>
+<body>
+    <h1>hello from cherki hamza wordpress theme development</h1>
+</body>
+</html>
